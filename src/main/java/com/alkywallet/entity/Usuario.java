e84f26c63019777e9245e2ef9847eb3925097299
guabit <guanmiflores@gmail.com>
2026-08-26 13:36:20 -0300

    Entidad de base de datos para Usuarios definida

---
+++ src/main/java/com/alkywallet/entity/Usuario.java
@@ -0,0 +1,49 @@
+package com.alkywallet.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "usuarios")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false)
+    private String apellido;
+
+    @Column(nullable = false, unique = true)
+    private String dni;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    /* Aca iria el rol
+    @Column(nullable = false)
+    private Roles rol;
+     */
+
+    @Column(name = "is_deleted", nullable = false)
+    private boolean isDeleted;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+}
