c6196658f057cc3fa3478736a9680d5e3fb81bf4
guabit <guanmiflores@gmail.com>
2026-08-25 21:14:38 -0300

    Esqueleto inicial del proyecto con estructura MVC, dependencias y docker-compose

---
+++ src/main/java/com/alkywallet/AlkyWalletApplication.java
@@ -0,0 +1,13 @@
+package com.alkywallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlkyWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlkyWalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alkywallet/AlkyWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alkywallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlkyWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
