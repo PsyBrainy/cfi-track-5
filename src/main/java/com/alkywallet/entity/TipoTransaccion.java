98f56ec83860d64fb8321213b4e106f12182478b
guabit <guanmiflores@gmail.com>
2026-09-04 06:35:26 -0300

    refactor(entity): aplicar anotaciones Lombok Getter y AllArgsConstructor en TipoTransaccion

--- src/main/java/com/alkywallet/entity/TipoTransaccion.java
+++ src/main/java/com/alkywallet/entity/TipoTransaccion.java
@@ -1,23 +1,15 @@
 package com.alkywallet.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum TipoTransaccion {
-    DEPOSITO(true, "Deposito"),
+    DEPOSITO(true, "Depósito"),
     INGRESO(true, "Transferencia Recibida"),
     EGRESO(false, "Transferencia Enviada");
 
     private final boolean ingreso;
     private final String descripcion;
-
-    TipoTransaccion(boolean ingreso, String descripcion) {
-        this.ingreso = ingreso;
-        this.descripcion = descripcion;
-    }
-
-    public boolean isIngreso() {
-        return ingreso;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
 }
