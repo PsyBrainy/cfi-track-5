556306ce2e395aa92340e492546e5e65128ed407
vanevila27 <66636851+vanevila27@users.noreply.github.com>
2026-09-04 13:54:30 -0300

    feat: actualiza vista de perfil, dashboard.javascript y estilos en dashboard.module.css

--- src/main/java/com/alkywallet/repository/UserRepository.java
+++ src/main/java/com/alkywallet/repository/UserRepository.java
@@ -3,7 +3,12 @@ package com.alkywallet.repository;
 import com.alkywallet.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<Usuario, Long> {
+    
+    // Agregamos la búsqueda por email y por dni
+    Optional<Usuario> findByEmail(String email);
+    Optional<Usuario> findByDni(String dni);
 }
